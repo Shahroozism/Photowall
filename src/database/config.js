42d2e4a1760efcba16051983386434d8637b0bc4
Shahrooz <49618285+Shahroozism@users.noreply.github.com>
2019-08-05 09:36:19 -0400

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
   var config = {
-    apiKey: "AIzaSyBFUPil0sR9yu1Em0slH_CF8O6Cp-TX4o4",
+    apiKey: ""
     authDomain: "photowall-65315.firebaseapp.com",
     databaseURL: "https://photowall-65315.firebaseio.com",
     projectId: "photowall-65315",
     storageBucket: "photowall-65315.appspot.com",
     messagingSenderId: "109640130787",
-    appId: "1:109640130787:web:df7c20f3acd448b8"
+    appId: ""
   };
 
 firebase.initializeApp(config)
